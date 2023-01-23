758b696d1b4dd8dce64c533e3a6cac21863f9658
alvan <alvan.ghon@gmail.com>
2023-01-23 15:37:18 +0700

    create new project MVC C#

---
+++ LearnHotelWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LearnHotelWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
