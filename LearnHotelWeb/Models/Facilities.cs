b045c375ffc346a124f7438f843ef35b7cfa50b6
alvan <alvan.ghon@gmail.com>
2023-01-25 11:38:46 +0700

    add model for Hotel and Facilities

---
+++ LearnHotelWeb/Models/Facilities.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LearnHotelWeb.Models
+{
+    public class Facilities
+    {
+        public int FaciId { get; set; }
+        public int FaciCagroId { get; set; }
+        public int FaciHotelId { get; set; }
+        public string FaciName { get; set; }
+        public string FaciDescription { get; set; }
+        public int FaciMaxNumber { get; set; }
+        public string FaciMeasureUnit { get; set; }
+        public string FaciRoomNumber { get; set; }
+        public DateOnly FaciStartDate { get; set; }
+        public DateOnly FaciEndDate { get; set; }
+        public decimal FaciLowPrice { get; set; }
+        public decimal FaciHighPrice { get; set; }
+        public decimal FaciRatePrice { get; set; }
+        public float FaciDiscount { get; set; }
+        public float FaciTaxRate { get; set; }
+        public DateTime FaciModifiedDate { get; set; }
+        
+    }
+}
---
+++ LearnHotelWeb/Models/Hotel.cs
@@ -0,0 +1,17 @@
+﻿
+namespace LearnHotelWeb.Models
+{
+    public class Hotel
+    {
+        public int HotelId { get; set; }
+
+        public string HotelName { get; set; }
+        public string HotelDescription { get; set; }
+        public short HotelRatingStar { get; set; }
+        public string HotelPhoneNumber { get; set; }
+        public DateTime HotelModifiedDate { get; set; } = DateTime.Now;
+        public int HotelAddressId { get; set; }
+
+
+    }
+}
